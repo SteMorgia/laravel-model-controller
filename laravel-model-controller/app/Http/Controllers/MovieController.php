fe31686e7b31556489b5349c7bd1dae800df8087
Stefano Morgia <ste.morgia@gmail.com>
2022-09-28 18:22:09 +0200

    bug server (da cambiare php.ini)

--- laravel-model-controller/app/Http/Controllers/MovieController.php
+++ laravel-model-controller/app/Http/Controllers/MovieController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    public function index() {
-        return view('movie-controller');
+    public function list() {
+        $movies = Movie::all();
+        return view('movie-list', compact('movies'));
+    }
 }
--- laravel-model-controller/resources/views/movie-list.blade.php
+++ laravel-model-controller/resources/views/movie-list.blade.php
@@ -5,6 +5,14 @@
 @section('content')
     <h1>Lista dei film:</h1>
 
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                {{$book->id}} - {{$book->title}} - {{$book->original_title}} - {{$book->nationality}} - {{$book->date}} - {{$book->vote}}
+            </li>
+        @endforeach
+    </ul>
+
     
 
 @endsection
